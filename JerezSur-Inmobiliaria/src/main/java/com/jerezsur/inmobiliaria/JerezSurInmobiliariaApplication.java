ca7bcd3f5e02b09a65ea294838cac78f3aae36f3
Orellana16 <davilitoorellana@gmail.com>
2026-01-14 14:42:33 +0100

    chore: initial spring boot setup with dependencies and folder structure

---
+++ JerezSur-Inmobiliaria/src/main/java/com/jerezsur/inmobiliaria/JerezSurInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.jerezsur.inmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerezSurInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerezSurInmobiliariaApplication.class, args);
+	}
+
+}
---
+++ JerezSur-Inmobiliaria/src/test/java/com/jerezsur/inmobiliaria/JerezSurInmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerezsur.inmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerezSurInmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
