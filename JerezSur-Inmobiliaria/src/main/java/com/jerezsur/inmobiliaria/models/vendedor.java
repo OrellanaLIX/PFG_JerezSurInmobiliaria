907c74ab0165dbeb7207f78ad9101d0ea94c1bae
David Orellana <daviorellana.o16@gmail.com>
2026-05-29 14:27:48 +0200

    feat: improvement in aesthetics of the admin dashboard

--- JerezSur-Inmobiliaria/src/main/java/com/jerezsur/inmobiliaria/models/vendedor.java
+++ JerezSur-Inmobiliaria/src/main/java/com/jerezsur/inmobiliaria/models/vendedor.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
