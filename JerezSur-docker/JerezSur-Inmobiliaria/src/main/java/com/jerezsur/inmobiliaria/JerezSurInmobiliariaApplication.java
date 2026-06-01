5aa4a29d8fd2eadcd4d9273d438c290cd79d09d5
David Orellana <daviorellana.o16@gmail.com>
2026-06-01 13:16:47 +0200

    feat: Creation of fakedata

--- JerezSur-docker/JerezSur-Inmobiliaria/src/main/java/com/jerezsur/inmobiliaria/JerezSurInmobiliariaApplication.java
+++ JerezSur-docker/JerezSur-Inmobiliaria/src/main/java/com/jerezsur/inmobiliaria/JerezSurInmobiliariaApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -25,6 +26,7 @@ public class JerezSurInmobiliariaApplication {
 	}
 
 	@Bean
+	@Order(1)
 	public CommandLineRunner initAdminUsuario(
 			UsuarioRepository usuarioRepository,
 			TrabajadorRepository trabajadorRepository,
